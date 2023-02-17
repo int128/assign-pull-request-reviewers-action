d32574383861325d3d2bd84a2ce4d7ae730cc0d4
Hidetake Iwata <int128@gmail.com>
2023-02-17 15:08:28 +0900

    Post comment of related pull requests (#310)
    * Post comment of related pull requests
    
    * format

---
+++ src/comment.ts
@@ -0,0 +1,44 @@
+import * as core from '@actions/core'
+import { Octokit, Repository } from './types'
+
+export const createOrUpdateComment = async (
+  octokit: Octokit,
+  repository: Repository,
+  pullNumber: number,
+  body: string
+) => {
+  const commentKey = `<!-- assign-pull-request-reviewers-action -->`
+
+  core.info(`#${pullNumber}: finding key ${commentKey}`)
+  const { data: comments } = await octokit.rest.issues.listComments({
+    owner: repository.owner,
+    repo: repository.repo,
+    issue_number: pullNumber,
+    sort: 'created',
+    direction: 'desc',
+    per_page: 100,
+  })
+  core.info(`#${pullNumber}: found ${comments.length} comment(s)`)
+  for (const comment of comments) {
+    if (comment.body?.includes(commentKey)) {
+      core.info(`#${pullNumber}: updating the comment ${comment.html_url}`)
+      const { data: updated } = await octokit.rest.issues.updateComment({
+        owner: repository.owner,
+        repo: repository.repo,
+        comment_id: comment.id,
+        body: `${body}\n${commentKey}`,
+      })
+      core.info(`#${pullNumber}: updated the comment ${updated.html_url}`)
+      return
+    }
+  }
+
+  core.info(`#${pullNumber}: creating a comment`)
+  const { data: created } = await octokit.rest.issues.createComment({
+    owner: repository.owner,
+    repo: repository.repo,
+    issue_number: pullNumber,
+    body: `${body}\n${commentKey}`,
+  })
+  core.info(`#${pullNumber}: created a comment as ${created.html_url}`)
+}
--- src/reconcile.ts
+++ src/reconcile.ts
@@ -1,26 +1,38 @@
 import * as core from '@actions/core'
+import { createOrUpdateComment } from './comment'
 import { extractReviewerUsers, PullRequestReviewGroup } from './group'
-import { Octokit, Repository } from './types'
+import { Octokit, PullRequest, Repository } from './types'
 
 export const reconcile = async (octokit: Octokit, repo: Repository, groups: PullRequestReviewGroup[]) => {
   for (const group of groups) {
+    const comment = [`## Related pull requests (${group.labels.join(', ')})`]
+    for (const pull of group.pulls) {
+      comment.push(`- #${pull.number}`)
+    }
+
     for (const pull of group.pulls) {
       if (pull.state !== 'open') {
         core.info(`#${pull.number}: state is ${pull.state}, skip`)
         continue
       }
-      const currentReviewers = new Set(extractReviewerUsers(pull))
-      const alreadyRequested = group.reviewers.every((r) => currentReviewers.has(r))
-      if (alreadyRequested) {
-        core.info(`#${pull.number}: already requested to ${group.reviewers.map((r) => `@${r}`).join()}, skip`)
-        continue
-      }
-      core.info(`#${pull.number}: requesting a review to ${group.reviewers.map((r) => `@${r}`).join()}`)
-      await octokit.rest.pulls.requestReviewers({
-        ...repo,
-        pull_number: pull.number,
-        reviewers: group.reviewers,
-      })
+
+      await createOrUpdateComment(octokit, repo, pull.number, comment.join('\n'))
+      await requestReview(octokit, repo, pull, group)
     }
   }
+}
+
+const requestReview = async (octokit: Octokit, repo: Repository, pull: PullRequest, group: PullRequestReviewGroup) => {
+  const currentReviewers = new Set(extractReviewerUsers(pull))
+  const alreadyRequested = group.reviewers.every((r) => currentReviewers.has(r))
+  if (alreadyRequested) {
+    core.info(`#${pull.number}: already requested to ${group.reviewers.map((r) => `@${r}`).join()}, skip`)
+    return
+  }
+  core.info(`#${pull.number}: requesting a review to ${group.reviewers.map((r) => `@${r}`).join()}`)
+  await octokit.rest.pulls.requestReviewers({
+    ...repo,
+    pull_number: pull.number,
+    reviewers: group.reviewers,
+  })
 }
