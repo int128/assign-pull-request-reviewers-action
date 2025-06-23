34e4f4fa85f9fefd1af9b1e7399fb3eaaa6adacb
Hidetake Iwata <int128@gmail.com>
2025-06-23 14:25:10 +0900

    Remove requesting review feature (#835)
    * Remove requesting review feature
    
    * Release v2

--- src/group.ts
+++ src/group.ts
@@ -34,36 +34,3 @@ export const computePullRequestGroups = (pulls: PullRequest[], labelPrefix: stri
   }
   return [...m.values()]
 }
-
-export type PullRequestReviewGroup = PullRequestGroup & {
-  reviewers: string[]
-}
-
-export const computePullRequestReviewGroups = (groups: PullRequestGroup[]): PullRequestReviewGroup[] => {
-  const reviewGroups: PullRequestReviewGroup[] = []
-  for (const group of groups) {
-    if (!group.pulls.some((pull) => pull.state === 'open')) {
-      continue
-    }
-    const reviewers = computePullRequestReviewers(group)
-    if (reviewers.length === 0) {
-      continue
-    }
-    reviewGroups.push({ ...group, reviewers })
-  }
-  return reviewGroups
-}
-
-const computePullRequestReviewers = (group: PullRequestGroup): string[] => {
-  // find the oldest reviewers
-  for (const pull of group.pulls) {
-    const reviewers = extractReviewerUsers(pull)
-    if (reviewers.length > 0) {
-      return reviewers
-    }
-  }
-  return []
-}
-
-export const extractReviewerUsers = (pull: PullRequest): string[] =>
-  pull.requested_reviewers?.filter((reviewer) => reviewer.type === 'User').map((reviewer) => reviewer.login) ?? []
--- src/reconcile.ts
+++ src/reconcile.ts
@@ -1,10 +1,10 @@
 import * as core from '@actions/core'
 import { createOrUpdateIssueBody, createOrUpdateIssueComment } from './issue.js'
-import { extractReviewerUsers, PullRequestReviewGroup } from './group.js'
-import { Octokit, PullRequest, Repository } from './types.js'
+import { PullRequestGroup } from './group.js'
+import { Octokit, Repository } from './types.js'
 import { formatComment, formatDashboard } from './format.js'
 
-export const reconcile = async (octokit: Octokit, repo: Repository, groups: PullRequestReviewGroup[]) => {
+export const reconcile = async (octokit: Octokit, repo: Repository, groups: PullRequestGroup[]) => {
   const dashboard = formatDashboard(groups)
   core.info(`Review dashboard:\n----\n${dashboard}\n----`)
   core.info(`Writing to issue`)
@@ -19,22 +19,6 @@ export const reconcile = async (octokit: Octokit, repo: Repository, groups: Pull
       }
 
       await createOrUpdateIssueComment(octokit, repo, pull.number, comment)
-      await requestReview(octokit, repo, pull, group)
     }
   }
-}
-
-const requestReview = async (octokit: Octokit, repo: Repository, pull: PullRequest, group: PullRequestReviewGroup) => {
-  const currentReviewers = new Set(extractReviewerUsers(pull))
-  const alreadyRequested = group.reviewers.every((r) => currentReviewers.has(r))
-  if (alreadyRequested) {
-    core.info(`#${pull.number}: already requested to ${group.reviewers.map((r) => `@${r}`).join()}, skip`)
-    return
-  }
-  core.info(`#${pull.number}: requesting a review to ${group.reviewers.map((r) => `@${r}`).join()}`)
-  await octokit.rest.pulls.requestReviewers({
-    ...repo,
-    pull_number: pull.number,
-    reviewers: group.reviewers,
-  })
 }
